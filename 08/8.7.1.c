89c98091e08460ba456b7e934737ace98177be36
AaronMing <29460116@qq.com>
2022-01-22 18:03:28 +0800

    ADD

---
+++ 08/8.7.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int *p,i,a[10];
+	p = a;
+	printf("please enter 10 integer numbers:");
+	for (i=0;i<10;i++) scanf("%d",p++);
+	for (i=0;i<10;i++,p++) printf("%d ",*p);
+	printf("\n");
+	return 0;
+}
