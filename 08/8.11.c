dd6f6e33b6b7943efee980b87b075f9d33b74c04
AaronMing <29460116@qq.com>
2022-01-28 16:53:57 +0800

    ADD

---
+++ 08/8.11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	int a[3][4] = {1,3,5,7,9,11,13,15,17,19,21,23};
+	printf("%d,%d\n",a, *a);
+
+	printf("%d,%d\n",a[0],*(a+0));
+	printf("%d,%d\n",&a[0],&a[0][0]);
+
+	printf("%d,%d\n",a[1],a+1);	
+	printf("%d,%d\n",&a[1][0],*(a+1) + 0);
+
+	printf("%d,%d\n",a[2],*(a+2));
+	printf("%d,%d\n",&a[2],a+2);	
+
+	printf("%d,%d\n",a[1][0],*(*(a+1) + 0));
+	printf("%d,%d\n", *a[2],*(*(a+2) + 0));
+	
+	return 0;
+}
